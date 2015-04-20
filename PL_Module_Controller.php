b347e827c913a747e3199d69da7966cf363b7d10
Milos Soskic <artificermil@gmail.com>
2015-04-20 19:06:21 +0100

    Fix typos

--- PL_Module_Controller.php
+++ PL_Module_Controller.php
@@ -12,7 +12,7 @@ class PL_Module_Controller {
 
 	public function register_hooks() {
 		add_action( 'wp', array( $this, 'handle_request' ), 10, 1 );
-		add_action( $module->get_name(), array( $this, 'handle_action' ), 10, 10 );
+		add_action( $this->module->get_slug(), array( $this, 'handle_action' ), 10, 10 );
 	}
 
 	public function handle_request() {
